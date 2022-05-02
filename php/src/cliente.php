2cb7488ed462df307135a089def83ae2c92bfd0a
kirito-bebop <kirito-bebop@github.es>
2022-05-02 17:12:08 +0200

    Subida I

--- php/src/cliente.php
+++ php/src/cliente.php
@@ -3,11 +3,93 @@ ini_set('soap.wsdl_cache_enabled',0);
 ini_set('soap.wsdl_cache_ttl',0);
 $url = 'http://localhost/servidor.php';
 $uri = 'http://localhost/clienteServidor';
+$servidor = 'db6';
+$bbdd = 'proyecto';
+$usuario = 'root';
+$password = 'root';
 
-try{
-    $cliente = new SoapClient(null,['location'=>$url,'uri'=>$uri, 'trace'=>true ]);
-    $mostrarNome = $cliente->mostraNome('1');
-    echo $mostrarNome;
-}catch(SoapFault $f){
-    die("ERROO: " . $f->getMessage());
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CLIENTE</title>
+</head>
+<body>
+    <form action="">
+        <label for="">ID:</label>
+        <input type="text" name="id">
+        <input type="submit" value="Mostrar-Nome" name="mostrar-nome">
+        <input type="submit" value="Mostrar-Nome-Descripcion" name="mostrar-nome-desc">
+        <input type="submit" value="Mostrar-Nome-Desc-Prezo" name="mostrar-tres-datos">
+        <br/>
+        <hr/>
+        <label for="">FAMILIA: </label>
+        <select name="familia" id="">
+            <?php
+                try{
+                    $conn = new PDO("mysql:host=$servidor;dbname=$bbdd;charset=utf8",$usuario,$password);
+                    $stat = $conn->prepare("SELECT * FROM familias");
+                    $stat->execute();
+                    echo "ENTRA";
+                    while($fila = $stat->fetch()){
+                        echo "<option name='".$fila[0]."'>".$fila[0]."</option>";
+                    }
+                }catch(PDOException $e){
+                    die("ERROR: ". $e->getMessage());
+                }
+            ?>
+        </select>
+        <input type="submit" value="Mostrar Familia" name="mostrar-familia">
+
+    </form>
+    <table>
+        
+<?php
+
+if(isset($_GET['mostrar-nome'])){
+    echo "<tr><th>Nome</th></tr>";
+    if($_GET['id'] != null){
+        $id = $_GET['id'];
+        try{
+            $cliente = new SoapClient(null,['location'=>$url,'uri'=>$uri, 'trace'=>true ]);
+            $mostrarNome = $cliente->mostraNome($id);
+            echo $mostrarNome;
+        }catch(SoapFault $f){
+            die("ERROO: " . $f->getMessage());
+        }
+    }
+}else if(isset($_GET['mostrar-tres-datos'])){
+    echo "<tr><th>Nome</th><th>Descripcion</th><th>Prezo</th></tr>";
+    if($_GET['id'] != null){
+        $id = $_GET['id'];
+        try{
+            $cliente = new SoapClient(null,['location'=>$url,'uri'=>$uri, 'trace'=>true]);
+            $nomeDesc = $cliente->mostrarTresDatos($id);
+            echo $nomeDesc;
+        }catch(SoapFault $f){
+            die('ERRO: ' . $f->getMessage());
+        }
+    }
+}else if(isset($_GET['mostrar-familia'])){
+    echo "<tr><th>Codigo</th><th>Nome</th></tr>";
+    if($_GET['familia'] != null){
+        $familia = $_GET['familia'];
+        try{
+            $cliente = new SoapClient(null, ['location'=>$url, 'uri'=>$uri, 'trace'=>true]);
+            $family = $cliente->mostrarFamilia($familia);
+            echo $familia;
+            echo $family;
+        }catch(SoapFault $f){
+            die("ERRO: " . $f->getMessage());
+        }
+    }
 }
+    
+?>  
+    </table>
+</body>
+</html>
--- php/src/servidor.php
+++ php/src/servidor.php
@@ -3,23 +3,79 @@ class mostraNome{
 
     public function mostraNome($id){
         $servidor = 'db6';
-            $bbdd = 'proyecto';
-            $usuario = 'root';
-            $password = 'root';
-            try{
-                $pdo = new PDO("mysql:host=$servidor;dbname=$bbdd;charset=utf8",$usuario, $password);
-                $pdoStatement = $pdo->prepare("SELECT * FROM productos WHERE id = ?");
-                $pdoStatement->bindParam(1,$id);
-                $pdoStatement->execute();
-                if($pdoStatement->rowCount() >= 1){
-                    $fila = $pdoStatement->fetch();
-                    return "NOME: ".$fila[1] ." <br>DESCRIPCION: ". $fila[3]."<br>PREZO: ". $fila[4];  // 0 é o numero, 1 o nome
-                }
-            }catch(PDOException $e){
-                return("ERROR: " . $e->getMessage());
+        $bbdd = 'proyecto';
+        $usuario = 'root';
+        $password = 'root';
+        try{
+            $pdo = new PDO("mysql:host=$servidor;dbname=$bbdd;charset=utf8",$usuario, $password);
+            $pdoStatement = $pdo->prepare("SELECT * FROM productos WHERE id = ?");
+            $pdoStatement->bindParam(1,$id);
+            $pdoStatement->execute();
+            if($pdoStatement->rowCount() >= 1){
+                $fila = $pdoStatement->fetch();
+                return "<tr><td>".$fila[1] ." </td></tr>";  // 0 é o numero, 1 o nome
             }
+        }catch(PDOException $e){
+            return("ERROR: " . $e->getMessage());
         }
-    
+    }
+
+
+    public function mostrarNomeDescripcion($id){
+        $servidor = 'db6';
+        $bbdd = 'proyecto';
+        $usuario = 'root';
+        $password = 'root';
+        try{
+            $pdo = new PDO("mysql:host=$servidor;dbname=$bbdd;charset=utf8",$usuario, $password);
+            $pdoStatement = $pdo->prepare("SELECT * FROM productos WHERE id = ?");
+            $pdoStatement->bindParam(1,$id);
+            $pdoStatement->execute();
+            if($pdoStatement->rowCount() >= 1){
+                $fila = $pdoStatement->fetch();
+                return "<tr><td>".$fila[1] ." </td><td>".$fila[3] ." </td></tr>";  // 0 é o numero, 1 o nome
+            }
+        }catch(PDOException $e){
+            return("ERROR: " . $e->getMessage());
+        }
+    }
+    public function mostrarTresDatos($id){
+        $servidor = 'db6';
+        $bbdd = 'proyecto';
+        $usuario = 'root';
+        $password = 'root';
+        try{
+            $pdo = new PDO("mysql:host=$servidor;dbname=$bbdd;charset=utf8",$usuario, $password);
+            $pdoStatement = $pdo->prepare("SELECT * FROM productos WHERE id = ?");
+            $pdoStatement->bindParam(1,$id);
+            $pdoStatement->execute();
+            if($pdoStatement->rowCount() >= 1){
+                $fila = $pdoStatement->fetch();
+                return "<tr><td>".$fila[1] ." </td><td>".$fila[3] ." </td><td>".$fila[4] ."€ </td></tr>";  // 0 é o numero, 1 o nome
+            }
+        }catch(PDOException $e){
+            return("ERROR: " . $e->getMessage());
+        }
+    }
+    public function mostrarFamilia($fam){
+        $servidor = 'db6';
+        $bbdd = 'proyecto';
+        $usuario = 'root';
+        $password = 'root';
+        try{
+            $pdo = new PDO("mysql:host=$servidor;dbname=$bbdd;charset=utf8",$usuario, $password);
+            $pdoStatement = $pdo->prepare("SELECT * FROM familias WHERE cod =?");
+            $pdoStatement->bindParam(1,$fam);
+            $pdoStatement->execute();
+            if($pdoStatement->rowCount() >=1){
+                $trs = $pdoStatement->fetch();
+                return "<tr><td>".$fila[0]."</td><td>".$fila[1]."</td></tr>";
+            }
+        }catch(PDOException $e){
+            return("ERROR: " . $e->getMessage());
+        }
+    }
+       
 }
 
 
